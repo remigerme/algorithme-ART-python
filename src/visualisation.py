e9cb7f3772c7edb83b7e79f20ccadbb622c2604a
remigerme <remi.germe@gmail.com>
2022-05-29 12:59:45 +0200

    Visualisation des résultats de simu nb rayons

--- src/visualisation.py
+++ src/visualisation.py
@@ -72,4 +72,41 @@ def plot_simu_schema_acces(fichiers):
     plt.legend()
     plt.show()
 
-plot_simu_init(["simulations/varier_initialisation/lapin/ecarts_200.txt", "simulations/varier_initialisation/slp/ecarts_200.txt"])
+
+def plot_simu_nb_rayons(fichiers, labels, tailles):
+    # Initialisation
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+    
+    couleurs = ["#8C3C12", "#CF4D18", "#FF541D", "#124295", "#1A5FD8", "#1DADFF"]
+    if len(fichiers) > len(couleurs):
+        raise Exception("Pas assez de couleurs")
+
+    for (i, fichier) in enumerate(fichiers):
+        with open(fichier, "r") as f:
+            s = f.read()
+        re_absc= re.compile("\((?P<n_t>[0-9]+), (?P<n_r>[0-9]+)\)")
+        re_moy = re.compile("moyen : (0\.[0-9]*)")
+        re_euc = re.compile("euclidienne : ([0-9]+\.[0-9]*)")
+        absc = [int(m.group("n_t")) * int(m.group("n_r")) / tailles[i] for m in re_absc.finditer(s)]
+        ords_moy = re_moy.findall(s)
+        ords_euc = re_euc.findall(s)
+        ords_moy = [float(s) for s in ords_moy]
+        ords_euc = [float(s) for s in ords_euc]
+        ax1.plot(absc, ords_moy, color=couleurs[i], label=labels[i])
+        ax2.plot(absc, ords_euc, color=couleurs[i], label=labels[i])
+
+    # Légende
+    fig.suptitle("Écart à l'image cible suivant différents nombres de rayons tracés\n1e6 itérations pour 150x150, 1e5 itérations pour les autres, initialisation noire", fontsize = 16)
+    ax1.set_ylabel("Écart moyen", fontsize = 14)
+    ax2.set_ylabel("Écart en norme euclidienne", fontsize = 14)
+    ax1.set_xlabel("Nombre de rayons tracés / taille de l'image", fontsize = 14)
+    ax2.set_xlabel("Nombre de rayons tracés / taille de l'image", fontsize = 14)
+    ax1.set_xlim(right = 0.45)
+    ax2.set_xlim(right = 0.45)
+    plt.legend(fontsize = 12)
+    plt.show()
+
+chemin = "simulations/varier_nb_rayons/"
+plot_simu_nb_rayons([chemin + "lapin/ecarts_150.txt", chemin + "lapin/ecarts_100.txt", chemin + "lapin/ecarts_50.txt", chemin + "slp/ecarts_150.txt", chemin + "slp/ecarts_100.txt", chemin + "slp/ecarts_50.txt"],
+                    ["lapin 150x150", "lapin 100x100", "lapin 50x50", "slp 150x150", "slp 100x100", "slp 50x50"],
+                    [150*150, 100*100, 50*50, 150*150, 100*100, 50*50])
