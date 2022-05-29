b70986882b5759d6f58c005e4c0d73a3e6ab1cfb
remigerme <remi.germe@gmail.com>
2022-05-29 11:41:13 +0200

    Correction d'erreurs & améliorations des regex

--- src/visualisation.py
+++ src/visualisation.py
@@ -13,12 +13,12 @@ def plot_simu_init(fichiers):
     for (i, fichier) in enumerate(fichiers):
         with open(fichier, "r") as f:
             s = f.read()
-        re_moyen = re.compile("moyen : [0-9].[0-9]*")
-        re_euc = re.compile("euclidienne : [0-9].[0-9]*")
+        re_moyen = re.compile("moyen : (0\.[0-9]*)")
+        re_euc = re.compile("euclidienne : ([0-9]+\.[0-9]*)")
         ords_moy = re_moyen.findall(s)
         ords_euc = re_euc.findall(s)
-        ords_moy = [float(s[8:]) for s in ords_moy]
-        ords_euc = [float(s[14:]) for s in ords_euc]
+        ords_moy = [float(s) for s in ords_moy]
+        ords_euc = [float(s) for s in ords_euc]
         label = "lapin" if i == 0 else "shepp logan phantom" # valable uniquement ici
         ax1.bar([e * i + k for k in range(4)], ords_moy, width = e, color=couleurs[i], label=label)
         ax2.bar([e * i + k for k in range(4)], ords_euc, width = e, color=couleurs[i], label=label)
@@ -45,12 +45,12 @@ def plot_simu_schema_acces(fichiers):
     for (i, fichier) in enumerate(fichiers):
         with open(fichier, "r") as f:
             s = f.read()
-        re_moyen = re.compile("moyen : [0-9].[0-9]*")
-        re_euc = re.compile("euclidienne : [0-9].[0-9]*")
+        re_moyen = re.compile("moyen : (0\.[0-9]*)")
+        re_euc = re.compile("euclidienne : ([0-9]+\.[0-9]*)")
         ords_moy = re_moyen.findall(s)
         ords_euc = re_euc.findall(s)
-        ords_moy = [float(s[8:]) for s in ords_moy]
-        ords_euc = [float(s[14:]) for s in ords_euc]
+        ords_moy = [float(s) for s in ords_moy]
+        ords_euc = [float(s) for s in ords_euc]
         
         # On regroupe par image, on juxtapose "succ" et "alea"
         for k in range(2):
@@ -62,7 +62,7 @@ def plot_simu_schema_acces(fichiers):
             ax2.bar([k * e + i], ords_euc[k], width = e, color=couleurs[k], label=label)
 
     # Légende
-    fig.suptitle("Écart à l'image cible suivant différents schémas d'accès aux rayons\n200*200px, 80*80 rayons, 1e6 itérations, init noire", fontsize = 16)
+    fig.suptitle("Écart à l'image cible suivant différents schémas d'accès aux rayons\n200*200px, 80*80 rayons, 1e6 itérations, initialisation noire", fontsize = 16)
     ax1.set_ylabel("Écart moyen", fontsize = 14)
     ax2.set_ylabel("Écart en norme 2", fontsize = 14)
     ax1.set_xticks([len(fichiers) * e / 4, 1 + len(fichiers) * e / 4])
@@ -72,4 +72,4 @@ def plot_simu_schema_acces(fichiers):
     plt.legend()
     plt.show()
 
-plot_simu_schema_acces(["simulations/varier_schema_acces/lapin/ecarts_200.txt", "simulations/varier_schema_acces/slp/ecarts_200.txt"])
+plot_simu_init(["simulations/varier_initialisation/lapin/ecarts_200.txt", "simulations/varier_initialisation/slp/ecarts_200.txt"])
