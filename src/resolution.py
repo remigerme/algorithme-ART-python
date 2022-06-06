b445048cad70c0d4a02bd8f2b20b0e043074ac77
remigerme <remi.germe@gmail.com>
2022-06-06 18:32:56 +0200

    Correction d'une erreur dans l'initialisation de l'ART (normalisation en NORME 2) & schéma d'accès optimisé

--- src/resolution.py
+++ src/resolution.py
@@ -1,17 +1,24 @@
 from numpy import vdot, zeros, where
 from numpy.linalg import norm
-from random import randint
+from random import randint, choices
 from image import tronquer
 
 
-def prochain_rayon(j, N_R, aleatoire):
+def prochain_rayon(j, N_R, aleatoire, P, les_rayons):
+    """
+    aleatoire : 0 pour schéma successif
+                1 pour aléatoire équiprobable
+                2 pour aléatoire optimisé :
+                    on prend le rayon j avec une probabilité
+                    de norme2(A[j])**2 / norme_euc(A) ** 2
+                    ces probabilités sont contenues dans la liste P
+    """
     # j est le dernier rayon utilisé
-    if not aleatoire:
+    if aleatoire == 0:
         return (j + 1) % N_R
-    j_ = randint(0, N_R - 1)
-    while j == j_:
-        j_ = randint(0, N_R - 1)
-    return j_
+    elif aleatoire == 1:
+        return randint(0, N_R - 1)
+    return choices(les_rayons, weights = P, k = 1)[0]
 
 
 def ART(f0, A, R, N_ITER, aleatoire, cst):
@@ -19,11 +26,16 @@ def ART(f0, A, R, N_ITER, aleatoire, cst):
     N_P = cst.L * cst.H # nombre de pixels
     # Les vecteurs normaux aux hyperplans sont les (lignes) A[j]
     # Il est pratique de les rendre unitaires
-    # On calcule la norme 1 de chaque ligne
+    # On calcule la norme 2 de chaque ligne
     N = zeros((N_R, N_P))
-    normes = norm(A, ord = 1, axis = 1)
+    normes = norm(A, ord = 2, axis = 1)
     for j in range(N_R):
         N[j] = A[j] / normes[j]
+    
+    # Pour le choix aléatoire optimisé des rayons
+    norme_A = norm(A)
+    P = [(normes[j] / norme_A) ** 2 for j in range(N_R)]
+    les_rayons = list(range(0, N_R))
     
     # On calcule aussi les projections orthogonales de 0 sur les hyperplans
     T = zeros((N_R, N_P))
@@ -40,6 +52,6 @@ def ART(f0, A, R, N_ITER, aleatoire, cst):
     j = 0
     for _ in range(N_ITER):
         f = f - vdot(f, N[j]) * N[j] + T[j]
-        j = prochain_rayon(j, N_R, aleatoire)
+        j = prochain_rayon(j, N_R, aleatoire, P, les_rayons)
     tronquer(f)
     return f
