8df218a8b48d5140a125ca5e7ec5c88ebb5f0637
remigerme <remi.germe@gmail.com>
2022-06-06 18:35:11 +0200

    Nouvelle simulation, vitesse de cv pour les 3 schémas d'accès

--- src/simu_vitesse_cv_schema_acces.py
+++ src/simu_vitesse_cv_schema_acces.py
@@ -37,7 +37,12 @@ def main():
 
     def ART_custom(f0, A, R, N_ITER, aleatoire, cst, chemin):
 
-        schema_acces = "alea" if aleatoire else "succ"
+        if aleatoire == 0:
+            schema_acces = "succ"
+        elif aleatoire == 1:
+            schema_acces = "alea"
+        else:
+            schema_acces = "alea_opti"
 
         fichier = chemin + "{}_{}_{}_{}".format(cst.L, cst.N_THETA, cst.N_RHO, schema_acces)
 
@@ -45,11 +50,16 @@ def main():
         N_P = cst.L * cst.H # nombre de pixels
         # Les vecteurs normaux aux hyperplans sont les (lignes) A[j]
         # Il est pratique de les rendre unitaires
-        # On calcule la norme 1 de chaque ligne
+        # On calcule la norme 2 de chaque ligne
         N = zeros((N_R, N_P))
-        normes = norm(A, ord = 1, axis = 1)
+        normes = norm(A, ord = 2, axis = 1)
         for j in range(N_R):
             N[j] = A[j] / normes[j]
+        
+        # Pour le choix aléatoire optimisé des rayons
+        norme_A = norm(A)
+        P = [(normes[j] / norme_A) ** 2 for j in range(N_R)]
+        les_rayons = list(range(0, N_R))
         
         # On calcule aussi les projections orthogonales de 0 sur les hyperplans
         T = zeros((N_R, N_P))
@@ -66,35 +76,38 @@ def main():
         j = 0
         for k in range(1, N_ITER + 1):
             f = f - vdot(f, N[j]) * N[j] + T[j]
-            j = prochain_rayon(j, N_R, aleatoire)
+            j = prochain_rayon(j, N_R, aleatoire, P, les_rayons)
             if sauvegarder_k(k):
                 enregistrer_image(f, cst.L, cst.H, fichier + "_{}.png".format(k))
         tronquer(f)
-        return f       
-
+        return f
 
     # Paramètres
     N_THETA, N_RHO = 60, 60
-    I, L, H = image_depuis_fichier("exemples/lapin/lapin_200.png")
+    I, L, H = image_depuis_fichier("exemples/slp/slp_200.png")
     cst = Donnees(L, H, N_THETA, N_RHO)
-    chemin = "simulations/varier_vitesse_cv_schema_acces/lapin/"
+    chemin = "simulations/varier_vitesse_cv_schema_acces_bis/slp/"
 
     # Initialisation
     A = tracer_rayons(cst)
     R = dot(A, I)
     print("Rayons tracés")
     ecarts = {}
-
+    N_ITER = 60 * N_THETA * N_RHO + 1 # 60 cycles
     # Schéma d'accès "successif"
     f0 = zeros(L * H)
-    f = ART_custom(f0, A, R, int(1e6), False, cst, chemin)
+    f = ART_custom(f0, A, R, N_ITER, False, cst, chemin)
     print("Image \"successive\" terminée")
-
 
     # Schéma d'accès aléatoire
     f0 = zeros(L * H)
-    f = ART_custom(f0, A, R, int(1e6), True, cst, chemin)
+    f = ART_custom(f0, A, R, N_ITER, True, cst, chemin)
     print("Image \"aléatoire\" terminée")
+
+    # Schéma d'accès aléatoire amélioré
+    f0 = zeros(L * H)
+    f = ART_custom(f0, A, R, N_ITER, 2, cst, chemin)
+    print("Image \"aléatoire améliorée\" terminée")
 
 
 def extraire_stats(fichier_ref, chemin, taille):
@@ -103,7 +116,7 @@ def extraire_stats(fichier_ref, chemin, taille):
     ecarts = {}
     
     fichiers = [f for f in listdir(chemin) if isfile(join(chemin, f))]
-    re_filtrer = re.compile("(?P<taille>[0-9]+)_(?P<n_t>[0-9]+)_(?P<n_r>[0-9]+)_(?P<alea>succ|alea)_(?P<n_iter>[0-9]+)\.png")
+    re_filtrer = re.compile("(?P<taille>[0-9]+)_(?P<n_t>[0-9]+)_(?P<n_r>[0-9]+)_(?P<alea>succ|alea|alea_opti)_(?P<n_iter>[0-9]+)\.png")
 
     for m in re_filtrer.finditer(" ".join(fichiers)):
         m_taille = int(m.group("taille"))
@@ -125,5 +138,5 @@ def extraire_stats(fichier_ref, chemin, taille):
 
 for image in ["slp", "lapin"]:
     fichier_ref = "exemples/{}/{}_{}.png".format(image, image, 200)
-    chemin = "simulations/varier_vitesse_cv_schema_acces/{}/".format(image)
+    chemin = "simulations/varier_vitesse_cv_schema_acces_bis/{}/".format(image)
     extraire_stats(fichier_ref, chemin, 200)
