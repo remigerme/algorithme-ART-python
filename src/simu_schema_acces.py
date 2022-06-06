ad912b55d7c79c24c1020d668fbc546fee007794
remigerme <remi.germe@gmail.com>
2022-06-06 18:38:31 +0200

    Reprise de simu schéma accès

--- src/simu_schema_acces.py
+++ src/simu_schema_acces.py
@@ -11,7 +11,7 @@ def main():
     N_THETA, N_RHO = 80, 80
     I, L, H = image_depuis_fichier("exemples/slp/slp_200.png")
     cst = Donnees(L, H, N_THETA, N_RHO)
-    chemin = "simulations/varier_schema_acces/slp/"
+    chemin = "simulations/varier_schema_acces_bis/slp/"
     nom_fichier = "slp_200_1e6_{}_{}_uni_.png".format(N_THETA, N_RHO)
 
     # Initialisation
@@ -22,17 +22,24 @@ def main():
 
     # Schéma d'accès "successif"
     f0 = zeros(L * H)
-    f = ART(f0, A, R, int(1e6), False, cst)
+    f = ART(f0, A, R, int(1e6), 0, cst)
     enregistrer_image(f, L, H, chemin + "succ_" + nom_fichier)
     ecarts["succ"] = (ecart_moyen(I, f, L * H), ecart_norme_euc(I, f))
     print("Image \"successive\" terminée")
 
     # Schéma d'accès aléatoire
     f0 = zeros(L * H)
-    f = ART(f0, A, R, int(1e6), True, cst)
+    f = ART(f0, A, R, int(1e6), 1, cst)
     enregistrer_image(f, L, H, chemin + "alea_" + nom_fichier)
     ecarts["alea"] = (ecart_moyen(I, f, L * H), ecart_norme_euc(I, f))
     print("Image \"aléatoire\" terminée")
+
+    # Schéma d'accès aléatoire optimisé
+    f0 = zeros(L * H)
+    f = ART(f0, A, R, int(1e6), 2, cst)
+    enregistrer_image(f, L, H, chemin + "alea_opti_" + nom_fichier)
+    ecarts["alea_opti"] = (ecart_moyen(I, f, L * H), ecart_norme_euc(I, f))
+    print("Image \"aléatoire optimisée\" terminée")
 
     with open(chemin + "ecarts_200.txt", "w") as f:
         s = ""
